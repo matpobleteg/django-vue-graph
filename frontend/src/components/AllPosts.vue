d6dd9bde6a06f05aa9b7c7e5d4478e2b05cb4d53
Matias Poblete <mat.pobleteg@gmail.com>
2021-11-09 09:23:14 -0300

    Primer Commit

---
+++ frontend/src/components/AllPosts.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h2>Recent posts</h2>
+    <PostList v-if="allPosts" :posts="allPosts" />
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList'
+import gql from 'graphql-tag'
+
+
+export default {
+  name: 'AllPosts',
+  components: {
+    PostList,
+  },
+  data () {
+    return {
+        allPosts: null,
+    }
+  },
+  async created () {
+    const posts = await this.$apollo.query({
+      query: gql`query {
+        allPosts {
+          title
+          subtitle
+          publishDate
+          published
+          metaDescription
+          slug
+          author {
+            user {
+              username
+              firstName
+              lastName
+            }
+          }
+          tags {
+            name
+          }
+        }
+      }`,
+    })
+    this.allPosts = posts.data.allPosts
+  },
+}
+</script>
---
+++ frontend/src/components/AuthorLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <router-link
+    :to="`/author/${author.user.username}`"
+    >
+    {{displayName}}
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'AuthorLink',
+    props: {
+        author: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        displayName () {
+            return (
+                this.author.user.firstname &&
+                this.author.user.lastname &&
+                `${this.author.user.firstname} ${this.author.user.lastname}`
+            ) || `${this.author.user.username}`
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
